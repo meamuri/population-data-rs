203b6f009cba7931068544b14495097080d5358b
meamuri <meammf@yandex.ru>
2017-07-27 20:01:19 +0300

    new app arch

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,73 @@
-pub fn run() {
+use computing::loader::{read_csv, combine_by_countries, select_useful};
+use computing::solve::{millionaires, population, top};
+use computing::saver::{save_millionaires, save_top, save_population};
+
+const POPULATION_LEVEL: f64 = 1_000_000.0;
+const TOP_N: usize = 5;
+const FILE_BOTH: &'static str = "data/unsd-citypopulation-year-both.csv";
+const FILE_DIFF: &'static str = "data/unsd-citypopulation-year-fm.csv";  
+
+pub fn app_millionaires() {
+    let file_path = String::from(FILE_BOTH);
     
+    // records: 17059
+    let records = match read_csv(&file_path){
+        Ok(r) => r,
+        Err(_) => { panic!("ooops") },
+    };
+    
+    let cities = select_useful(records);                // cities: 4501
+    let countries = combine_by_countries(cities);       // countries: 208
+
+    let res_millionaires = millionaires(&countries, POPULATION_LEVEL);
+    save_millionaires(&res_millionaires);    
 }
 
-pub fn sub_app() {
-    println!("im here!");
+pub fn app_population() {
+    let file_path = String::from(FILE_BOTH);
+    
+    // records: 17059
+    let records = match read_csv(&file_path){
+        Ok(r) => r,
+        Err(_) => { panic!("ooops") },
+    };
+    
+    let cities = select_useful(records);                // cities: 4501
+    let countries = combine_by_countries(cities);       // countries: 208
+
+    let res_population = population(&countries);    
+    save_population(&res_population);
+}
+
+pub fn app_ratio() {
+    let file_path = String::from(FILE_DIFF);
+    
+    // records: 17059
+    let records = match read_csv(&file_path){
+        Ok(r) => r,
+        Err(_) => { panic!("ooops") },
+    };
+    
+    let cities = select_useful(records);                // cities: 4501
+    let countries = combine_by_countries(cities);       // countries: 208
+
+    // for (key, val) in countries.iter.take(5) {
+    //     println!(&key);
+    // }
+}
+
+pub fn app_top() {
+    let file_path = String::from(FILE_BOTH);
+    
+    // records: 17059
+    let records = match read_csv(&file_path){
+        Ok(r) => r,
+        Err(_) => { panic!("ooops") },
+    };
+    
+    let cities = select_useful(records);                // cities: 4501
+    let countries = combine_by_countries(cities);       // countries: 208
+
+    let res_top = top(&countries, TOP_N);
+    save_top(&res_top);    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,21 @@
 extern crate population_data_rs;
 
 use std::env;
-use population_data_rs::computing::loader::{read_csv, combine_by_countries, select_useful};
-use population_data_rs::computing::solve::{millionaires, population, top};
-use population_data_rs::computing::saver::{save_millionaires, save_top, save_population};
-use population_data_rs::app::sub_app;
-
-const POPULATION_LEVEL: f64 = 1_000_000.0;
-const TOP_N: usize = 5;
+use population_data_rs::app::{app_millionaires, app_population, app_ratio, app_top};
 
 fn main() {
     let args: Vec<String> = env::args().collect();    
-    if args.len() > 0 && args[1] == "diff" {
-        sub_app();
+    if args.len() < 2 {
+        println!("запустите программу с одним из аргументов командной строки:");
+        println!("millionaires population ratio top");
         return;
     }
 
-    let file_path = String::from("data/unsd-citypopulation-year-both.csv");
-    
-    // records: 17059
-    let records = match read_csv(&file_path){
-        Ok(r) => r,
-        Err(_) => { panic!("ooops") },
-    };
-    
-    let cities = select_useful(records);                // cities: 4501
-    let countries = combine_by_countries(cities);       // countries: 208
-
-    
-    let res_millionaires = millionaires(&countries, POPULATION_LEVEL);
-    let res_population = population(&countries);    
-    let res_top = top(&countries, TOP_N);
-    
-    save_millionaires(&res_millionaires);
-    save_top(&res_top);
-    save_population(&res_population);
+    match &args[1][..] {
+        "millionaires" => app_millionaires(),
+        "population" => app_population(),
+        "ratio" => app_ratio(),
+        "top" => app_top(),
+        _ => println!("incorrect args!"),
+    }
 }
